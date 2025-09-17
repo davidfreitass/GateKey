109c42653b7f5bd038f9b769140b8dad766b81b1
davidfreitass <davidcnt@outlook.com>
2025-09-17 15:01:19 -0300

    sprint boot project

---
+++ src/main/java/com/project/gatekey/GatekeyApplication.java
@@ -0,0 +1,13 @@
+package com.project.gatekey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeyApplication.class, args);
+	}
+
+}
