17203a8231c3afe0e9c2911ca077a2c01b036c1d
Rick Santos <rs6545570@gmail.com>
2025-09-18 17:35:47 -0300

    Implementando Entidades

--- src/main/java/br/com/gatekey/repositories/DispositivoRepository.java
+++ src/main/java/br/com/gatekey/repositories/DispositivoRepository.java
@@ -1,4 +1,10 @@
 package br.com.gatekey.repositories;
 
-public class DispositivoRepository {
+
+import br.com.gatekey.entities.Dispositivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DispositivoRepository extends JpaRepository<Dispositivo, Integer> {
 }
--- src/main/java/br/com/gatekey/repositories/RegistroAcessoRepository.java
+++ src/main/java/br/com/gatekey/repositories/RegistroAcessoRepository.java
@@ -1,4 +1,10 @@
 package br.com.gatekey.repositories;
 
-public class RegistroAcessoRepository {
+
+import br.com.gatekey.entities.RegistroAcesso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistroAcessoRepository extends JpaRepository<RegistroAcesso, Integer> {
 }
