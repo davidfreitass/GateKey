45fdb01bf2645489eccda2818920e5dec9bd2c31
CoutinnhoGC <contactcoutinho506@gmail.com>
2025-09-18 16:52:43 -0300

    Implementando entidade Morador com repository, application e facade

--- src/main/java/br/com/gatekey/repositories/MoradorRepository.java
+++ src/main/java/br/com/gatekey/repositories/MoradorRepository.java
@@ -1,4 +1,15 @@
 package br.com.gatekey.repositories;
 
-public class MoradorRepository {
+import br.com.gatekey.entities.Morador;
+
+import java.util.List;
+
+public interface MoradorRepository {
+
+    void salvar(Morador morador);
+    Morador buscarPorId(int id);
+    List<Morador> buscarTodos();
+    void atualizar(Morador morador);
+    void remover(int id);
+
 }
