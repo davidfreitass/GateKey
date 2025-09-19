cb127e3427c695e642901d0398113dc7a6a25f07
CoutinnhoGC <contactcoutinho506@gmail.com>
2025-09-18 21:28:03 -0300

    Implementando Entidade Morador

--- src/main/java/br/com/gatekey/facades/MoradorFacade.java
+++ src/main/java/br/com/gatekey/facades/MoradorFacade.java
@@ -1,4 +1,36 @@
 package br.com.gatekey.facades;
 
+import br.com.gatekey.applications.MoradorApplication;
+import br.com.gatekey.entities.Morador;
+import br.com.gatekey.repositories.MoradorRepository;
+
+import java.util.List;
+
 public class MoradorFacade {
+
+    private final MoradorApplication application;
+
+    public MoradorFacade(MoradorApplication application) {
+        this.application = application;
+    }
+
+    public void cadastrar(Morador morador) {
+        application.cadastrar(morador);
+    }
+
+    public Morador buscarPorId(int id) {
+        return application.buscarPorId(id);
+    }
+
+    public List<Morador> listarTodos() {
+        return application.listarTodos();
+    }
+
+    public void atualizar(Morador morador) {
+        application.atualizar(morador);
+    }
+
+    public void remover(int id) {
+        application.remover(id);
+    }
 }
