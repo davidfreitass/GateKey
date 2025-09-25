3dfbf37559965943dc1d0b27bdd855c833e5391a
CoutinnhoGC <contactcoutinho506@gmail.com>
2025-09-25 16:58:14 -0300

    adicionar CRUD de Morador com controller, facade, application e repository

---
+++ src/main/java/br/com/gatekey/models/MoradorModel.java
@@ -0,0 +1,67 @@
+package br.com.gatekey.models;
+
+public class MoradorModel {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private byte[] fotoPerfil;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte[] getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(byte[] fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
