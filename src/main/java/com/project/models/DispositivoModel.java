d183bd2898c736f4d67bfbc95f40d9f12ba02066
davidfreitass <davidcnt@outlook.com>
2025-12-09 23:12:26 -0300

    update table names

--- src/main/java/com/project/models/DispositivoModel.java
+++ src/main/java/com/project/models/DispositivoModel.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-    @Table(name = "Dispositivo")
+    @Table(name = "dispositivos")
     public class DispositivoModel {
 
             @Id
